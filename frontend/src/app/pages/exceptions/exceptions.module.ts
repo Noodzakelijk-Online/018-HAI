6233ce01a3d663887b68174d6fa52c62845ddaea
Saad Saleem <Saadullahsaleem7@gmail.com>
2026-07-07 06:40:37 +0500

    feat(frontend): exception-based workflow dashboard (phase 109)

---
+++ frontend/src/app/pages/exceptions/exceptions.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { ExceptionsComponent } from './exceptions.component';
+
+const routes: Routes = [{ path: '', component: ExceptionsComponent }];
+
+@NgModule({
+  declarations: [ExceptionsComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    NzTableModule,
+    NzCardModule,
+    NzTagModule,
+    NzEmptyModule,
+  ],
+})
+export class ExceptionsModule {}
